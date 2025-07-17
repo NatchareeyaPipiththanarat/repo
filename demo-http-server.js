bafe119d29ad3d06012bccc5879cb640f7f1ffd5
66070256 <66070256@kmitl.ac.th>
2025-07-17 14:26:11 +0700

    Add /about and /contact routes

--- demo-http-server.js
+++ demo-http-server.js
@@ -4,7 +4,15 @@ const port = 3000;
 const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello, World!\n');
+
+        if (req.url === '/about'){
+            req.end('About Page\n')
+        }else if(req.url === '/contact'){
+            req.end('Contact Page\n')
+        }else{
+            res.end('Hello, World!\n');
+        }
+
 });
 
 server.listen(port, hostname,() => {
