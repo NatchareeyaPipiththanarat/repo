9f131e9e35fdb9bd0442a003fac7bfa16e0ce233
66070256 <66070256@kmitl.ac.th>
2025-07-17 14:42:46 +0700

    Add /api route with JSON response

--- demo-http-server.js
+++ demo-http-server.js
@@ -1,15 +1,31 @@
+const { timeStamp } = require('console');
 const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
+        
+        
 
         if (req.url === '/about'){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
             req.end('About Page\n')
+
         }else if(req.url === '/contact'){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
             req.end('Contact Page\n')
+        }else if (req.url === '/api'){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/plain');
+            const data = {
+                    message: 'Hello from API',
+                    timestamp: new Date().toDateString()
+            };
+            res.end(JSON.stringify(data));
         }else{
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
             res.end('Hello, World!\n');
         }
 
